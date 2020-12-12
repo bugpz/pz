521619ee45b79a3577a1e479e2dce3de0da35549
bugpz <13558622779@163.com>
2020-12-12 18:50:45 +0800

    合并上传文件和注册的跳转js

--- yy/static/UI/js/jump.js
+++ yy/static/UI/js/jump.js
@@ -1,12 +1,34 @@
-onload = function jump() {
-    setInterval(go,1000)
-};
+var s1;
+var s2;
 var x = 4;
-function go() {
-    if(x >= 0){
-        document.getElementById('out_time').innerText=x;
-    }else {
-        location.href='/index/';
+var y = 3;
+var out_time = document.getElementById("out_time")
+var out_time_w = document.getElementById("wait_out_time")
+
+onload = function jump() {
+     s1 = setInterval(upload_out_time, 1000);
+     s2 = setInterval(wait_out_time, 1000)
+};
+
+
+function upload_out_time() {
+    if (!out_time) {
+        clearInterval(s1)
+    } else if (out_time && x >= 0) {
+        out_time.innerText = x;
+    } else {
+        location.href = '/index/';
     }
     x--;
+}
+
+function wait_out_time() {
+    if (!out_time_w) {
+        clearInterval(s2)
+    } else if (out_time_w && y >= 0) {
+        out_time_w.innerText = y;
+    } else {
+        location.href = "/login/"
+    }
+    y--;
 }
