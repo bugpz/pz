83a4f6fadd782e514e9793778e3609d4fcfc76ae
bugpz <13558622779@163.com>
2020-12-14 15:53:56 +0800

    修改文件上传模块

--- yy/static/UI/js/upload.js
+++ yy/static/UI/js/upload.js
@@ -4,7 +4,5 @@ onload = function Creatinput() {
     $('form').append('<input type="file" name="xFile" id="fileupload" multiple>' +
         '<input type="submit" value="上传" id="test">')
 }
-function add() {
-    $('form').append('<input type="file" name="xFile" id="upload_input">')
-}
+
 
