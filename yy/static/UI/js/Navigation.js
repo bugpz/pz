6d225d2ff4316f831c514fdec46f4dfb0167c56f
bugpz <13558622779@163.com>
2020-12-25 16:49:19 +0800

    替换var,优化代码格式问题

--- yy/static/UI/js/Navigation.js
+++ yy/static/UI/js/Navigation.js
@@ -1,29 +1,30 @@
 
 onload = function navigation() {
-    d = document.getElementById('Navigation')
-    var body = {'https://github.com/':'github',
-                'https://www.v2ex.com/':'V2EX',
-                'https://www.aliyun.com/':'aliyun',
-                'https://cloud.tencent.com/':'cloudqq',
-                'https://cloud.baidu.com/':'baiduyun',
-                'https://sso.qiniu.com/':'qiniu',
-                'https://v.qq.com/':'vqq',
-                'https://www.iqiyi.com/':'iqiyi',
-                'https://www.youku.com/':'youku',
-                'https://www.douyu.com/':'douyu',
-                'https://www.huya.com/':'huya',
-                'https://www.bilibili.com/':'bilibili',
-                'https://www.jianshu.com/"':'jianshu',
-                'https://www.oschina.net/':'oschina',
-                'http://www.myzaker.com/':'zaker',
-                'https://www.jd.com/':'jd',
-                'http://ifkdy.com/':'ifkdy',
-                'https://www.dytt8.net/':'dytt8',
-                'https://imgurl.org/':'imgurl',
-                'https://www.processon.com/':'processon',
-                'http://www.xiachufang.com/':'xiachufang'
-                }
-    for(var i in body){
+    let d = document.getElementById('Navigation')
+    let body = {
+        'https://github.com/': 'github',
+        'https://www.v2ex.com/': 'V2EX',
+        'https://www.aliyun.com/': 'aliyun',
+        'https://cloud.tencent.com/': 'cloudqq',
+        'https://cloud.baidu.com/': 'baiduyun',
+        'https://sso.qiniu.com/': 'qiniu',
+        'https://v.qq.com/': 'vqq',
+        'https://www.iqiyi.com/': 'iqiyi',
+        'https://www.youku.com/': 'youku',
+        'https://www.douyu.com/': 'douyu',
+        'https://www.huya.com/': 'huya',
+        'https://www.bilibili.com/': 'bilibili',
+        'https://www.jianshu.com/"': 'jianshu',
+        'https://www.oschina.net/': 'oschina',
+        'http://www.myzaker.com/': 'zaker',
+        'https://www.jd.com/': 'jd',
+        'http://ifkdy.com/': 'ifkdy',
+        'https://www.dytt8.net/': 'dytt8',
+        'https://imgurl.org/': 'imgurl',
+        'https://www.processon.com/': 'processon',
+        'http://www.xiachufang.com/': 'xiachufang'
+    };
+    for(let i in body){
         $(d).append('<div style="width: 25%; float: left;"><a href='+i +' '+' target="_blank">\n' +
         '        <img style="width: 32px; height: 32px;" src="/static/UI/icon/'+body[i]+'.png\  ">\n' +
         '        <span data-v-ed292c2c="">'+body[i]+'</span>\n' +
--- yy/static/UI/js/base.js
+++ yy/static/UI/js/base.js
@@ -1,17 +1,17 @@
 function bg_color() {
-    var bg_col_now = document.getElementById('nr').style.backgroundColor
-    var bg_cor_now1 = bg_col_now.substring(4,bg_col_now.length-1).replace(/\s/g," ")
-    var y = bg_cor_now1.split(",")
-    var r = parseInt(y[0]).toString(16).toUpperCase()
-    var g = parseInt(y[1]).toString(16).toUpperCase()
-    var b = parseInt(y[2]).toString(16).toUpperCase()
-    var rgb = "#"+r+g+b
-    if(rgb ==='#F0F8FF'){
+    let bg_col_now = document.getElementById('nr').style.backgroundColor
+    let bg_cor_now1 = bg_col_now.substring(4, bg_col_now.length - 1).replace(/\s/g, " ")
+    let y = bg_cor_now1.split(",")
+    let r = parseInt(y[0]).toString(16).toUpperCase()
+    let g = parseInt(y[1]).toString(16).toUpperCase()
+    let b = parseInt(y[2]).toString(16).toUpperCase()
+    let rgb = "#" + r + g + b
+    if (rgb === '#F0F8FF') {
         bg_col_now = '#22303f'
-    }
-    else {
+    } else {
         bg_col_now = '#F0F8FF'
     }
     document.getElementById('nr').style.backgroundColor = bg_col_now;
 }
-  // 颜色表达式 rgb(xx, xx, xx) 为十进制  一组3个  每个范围0-255   对应#xxxxxx   x为2个一组  十六进制
+
+// 颜色表达式 rgb(xx, xx, xx) 为十进制  一组3个  每个范围0-255   对应#xxxxxx   x为2个一组  十六进制
