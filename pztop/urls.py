c90f04b895d7ef7cbfb3a810391b858d46e26652
bugpz <13558622779@163.com>
2021-03-07 21:57:42 +0800

    fix：增加接口配置

--- pztop/urls.py
+++ pztop/urls.py
@@ -13,31 +13,48 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-#coding:utf-8
+# coding:utf-8
 from django.contrib import admin
 from django.conf.urls import url
-from django.urls import path,re_path,include
+from django.urls import path, re_path, include
 from django.conf.urls.static import static
 from django.conf import settings
 from django.views.generic.base import RedirectView
 from yy import views
 import xadmin
-urlpatterns = [
-    # path('admin/', admin.site.urls),
-    path('xadmin/', xadmin.site.urls),
-    path(r'favicon.ico', RedirectView.as_view(url=r'static/UI/img/favicon.ico')),
-    path(r'', views.index),
-    path(r'wait/', views.wait),
-    path(r'register/', views.register),
-    path(r'login/', views.login),
-    path(r'logout/', views.logout),
-    path(r'index/', views.index),
-    path(r'uploadFiles/', views.uploadFiles),
-    path(r'robots.txt/', views.robots),
-    path(r'Navigation/', views.Navigation),
+from rest_framework import routers  # 路由配置模块
 
-    # 富文本编辑器
-    path('ckeditror/', include('ckeditor_uploader.urls')),
-    # path(r'article/',views.article),
-    url(r'^blog/', include('yy.urls', namespace='yy'))
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# 路由
+router = routers.DefaultRouter()
+# router.register(r'users', views.UserViewSet, base_name='user')
+router.register(r'groups', views.GroupViewSet)
+
+# 重要的是如下三行
+from rest_framework.schemas import get_schema_view
+from rest_framework_swagger.renderers import SwaggerUIRenderer, OpenAPIRenderer
+
+schema_view = get_schema_view(title='Users API', renderer_classes=[OpenAPIRenderer, SwaggerUIRenderer])
+
+urlpatterns = [
+                  # path('admin/', admin.site.urls),
+                  path('xadmin/', xadmin.site.urls),
+                  path(r'favicon.ico', RedirectView.as_view(url=r'static/UI/img/favicon.ico')),
+                  # path(r'', views.index),
+                  path('', include(router.urls)),
+                  path(r'wait/', views.wait),
+                  path(r'register/', views.register),
+                  path(r'login/', views.login),
+                  path(r'logout/', views.logout),
+                  path(r'index/', views.index),
+                  path(r'uploadFiles/', views.uploadFiles),
+                  path(r'robots.txt/', views.robots),
+                  path(r'Navigation/', views.Navigation),
+                  path('api-auth', include('rest_framework.urls', namespace='rest_framework')),
+                  path('swagger', schema_view, name='docs'),
+
+                  # 富文本编辑器
+                  path('ckeditror/', include('ckeditor_uploader.urls')),
+                  # path(r'article/',views.article),
+                  url(r'^blog/', include('yy.urls', namespace='yy')),
+                  path('api/', include('yy.urls'))
+              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
