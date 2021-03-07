d3a6ed9feb07db4fe73f1dfd73ceeeafb90d4978
bugpz <13558622779@163.com>
2021-03-07 21:57:54 +0800

    fix：增加接口配置

--- yy/urls.py
+++ yy/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls import url
+from django.urls import path
 from . import views
 
 app_name = 'yy'
 urlpatterns = [
     url(r'^article/(?P<article_id>[0-9]+)$', views.articleinfo, name='page'),
+    path('register', views.register, name='register'),
+    path('test', views.test, name='test')
 ]
