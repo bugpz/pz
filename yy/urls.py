ee435f126fed1b3d9fb38fe398068d6fd4cf23e0
bugpz <13558622779@163.com>
2021-03-09 17:12:20 +0800

    fix：子路由增加注册

--- yy/urls.py
+++ yy/urls.py
@@ -1,12 +1,14 @@
 from django.conf.urls import url
 from django.urls import path
 from . import views
+from yy.API import uesr
 
 app_name = 'yy'
 urlpatterns = [
     # url(r'^article/(?P<article_id>[0-9]+)$', views.articleinfo, name='page'),
-    path('register', views.register, name='register'),
+    # path('register', views.register, name='register'),
     path('test', views.test, name='test'),
     path('test1', views.test1, name='t1'),
-    path('t2', views.Test1.as_view())
+    path('t2', views.Test1.as_view()),
+    path('register', uesr.Register.as_view())
 ]
