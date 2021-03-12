0d5fee466e415f3db1d5c50ba665c2120f6f2359
bugpz <13558622779@163.com>
2021-03-12 17:13:10 +0800

    fix：路由增加登录接口

--- yy/urls.py
+++ yy/urls.py
@@ -1,7 +1,7 @@
-from django.conf.urls import url
+# from django.conf.urls import url
 from django.urls import path
 from . import views
-from yy.API import uesr
+from yy.API import user
 
 app_name = 'yy'
 urlpatterns = [
@@ -10,5 +10,7 @@ urlpatterns = [
     path('test', views.test, name='test'),
     path('test1', views.test1, name='t1'),
     path('t2', views.Test1.as_view()),
-    path('register', uesr.Register.as_view())
+    path('register', user.Register.as_view()),
+    path('login', user.Login.as_view())
+    # path(r'^ register/', user.Register.as_view(), name='task')
 ]
